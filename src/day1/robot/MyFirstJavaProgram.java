c8d9a9a0a8c39f65c9b31cfc9aa817c4a04ebef3
TJHL <league@WTS14.attlocal.net>
2016-06-28 14:58:11 -0700

    Learned Eclipse

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot Clank= new Robot();
+Clank.setRandomPenColor();
+Clank.penDown();
+	Clank.setPenWidth(100);	
+	for (int i = 0; i <20; i++) {
+		Clank.move(400);
+	Clank.turn(90);
+	Clank.setRandomPenColor();
+	}
 	
-		
-		
+	
+	
 	}
 }
